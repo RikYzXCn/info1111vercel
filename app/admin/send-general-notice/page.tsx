c00986445153a96e9e6e3187cfab41d5f388243a
RikYzXCn <2251098734@qq.com>
2025-05-31 14:15:04 +1000

    php back

--- app/admin/send-general-notice/page.tsx
+++
@@ -1,9 +0,0 @@
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
-import SendGeneralNoticeClient from './SendGeneralNoticeClient';
-
-export default function SendGeneralNoticePage() {
-  const token = (cookies() as any).get('auth_token')?.value;
-  if (token !== 'admin-logged-in') redirect('/my-account');
-  return <SendGeneralNoticeClient />;
-}
